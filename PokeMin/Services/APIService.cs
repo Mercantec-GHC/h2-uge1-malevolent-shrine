1a58064f9831a400d9b87efd805cf572a4d12e9c
3xterr <177241359+3xterr@users.noreply.github.com>
2025-08-06 15:30:54 +0200

    fix: pics no longer take forever to load in pokemon list feat: user can now enter desired page number to jump to + styles for pagination bar

--- PokeMin/Services/APIService.cs
+++ PokeMin/Services/APIService.cs
@@ -7,33 +7,17 @@ namespace PokeMin.Services
     public class APIService
     {
         private readonly HttpClient _httpClient;
-        //       private const string BaseUrl = "";
         public APIService(HttpClient httpClient)
         {
             _httpClient = httpClient;
         }
 
-        public async Task<List<PokemonCard>> GetAllPokemons(int offset = 0, int limit = 10)
+        public async Task<List<Pokemon>> GetAllPokemonsAsync(int offset  = 0, int limit = 10)
         {
             var listResponse = await _httpClient.GetFromJsonAsync<PokemonResponse>($"https://pokeapi.co/api/v2/pokemon?limit={limit}&offset={offset}");
             if (listResponse == null) return [];
 
-            var tasks = listResponse.Results.Select(async entry =>
-            {
-                var detail = await _httpClient.GetFromJsonAsync<PokemonDetailsData>($"https://pokeapi.co/api/v2/pokemon/{entry.Name}");
-                if (detail == null) return null;
-
-                return new PokemonCard
-                {
-                    Name = detail.Name,
-                    ImageUrl = detail.Sprites.Front_Default,
-                    Types = detail.Types.Select(t => t.Type.Name).ToList()
-
-                };
-            });
-
-            var cards = await Task.WhenAll(tasks);
-            return cards.Where(c => c != null).ToList()!;
+            return listResponse.Results.ToList();
         }
 
         public async Task<PokemonCard?> GetPokemonDetailAsync(string nameOrId)
