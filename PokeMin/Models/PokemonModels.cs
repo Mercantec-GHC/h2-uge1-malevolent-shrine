87b665fe90d9d21e0d877afc52858b1f43855ea4
3xterr <177241359+3xterr@users.noreply.github.com>
2025-08-07 08:56:59 +0200

    solved merge conflicts + added navigation to the pokemon game

--- PokeMin/Models/PokemonModels.cs
+++ PokeMin/Models/PokemonModels.cs
@@ -11,29 +11,6 @@ namespace PokeMin.Models
         public int Weight { get; set; }
     }
 
-    public class PokemonDetailsData
-    {
-        public string Name { get; set; }
-        public int Height { get; set; }
-        public int Weight { get; set; }
-        public Sprites Sprites { get; set; }
-        public List<PokemonType> Types { get; set; }
-    }
-    public class Sprites
-    {
-        [JsonPropertyName("front_default")]
-        public string Front_Default { get; set; }
-    }
-    public class PokemonType
-    {
-        public TypeInfo Type { get; set; }
-    }
-
-    public class TypeInfo
-    {
-        public string Name { get; set; }
-    }
-
     public class Pokemon
     {
         public string Name { get; set; }
